e9a50149eb1068dc640a0b9bda150f091717e941
fady <fady@DESKTOP-MDQ512F>
2026-01-26 15:38:24 +0200

    Create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('NewProjectForWorkAngular');
+  title = 'NewProjectForWorkAngular';
+  count = signal(0);
 }
